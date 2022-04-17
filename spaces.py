bd89c4ab2441511889a0dcaae99167a17e718a24
Abhinov <75292530+ippudkippude@users.noreply.github.com>
2022-04-17 12:08:23 +0530

    Update spaces.py
    added def

--- spaces.py
+++ spaces.py
@@ -1,9 +1,10 @@
 import    os
 import      sys
 
-print("First line with spaces" )
-# Comment with 1 tab and 4 spaces
-if 1<2:
+def firstDef():
+    print("First line with spaces" )
+    # Comment with 1 tab and 4 spaces
+    if 1<2:
     print("Inside if but with tab+4 spaces")
-                                #     Comment with 1 tab and 4 spaces
-        
+                            #     Comment with 1 tab and 4 spaces
+
