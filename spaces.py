0f0c6d2ccd6e3ff3e6acf9dad768a5bf3c8cccf3
Abhinov <75292530+ippudkippude@users.noreply.github.com>
2022-04-17 12:09:23 +0530

    Update spaces.py

--- spaces.py
+++ spaces.py
@@ -5,6 +5,6 @@ def firstDef():
     print("First line with spaces" )
     # Comment with 1 tab and 4 spaces
     if 1<2:
-    print("Inside if but with tab+4 spaces")
+            print("Inside if but with tab+4 spaces")
                             #     Comment with 1 tab and 4 spaces
 
